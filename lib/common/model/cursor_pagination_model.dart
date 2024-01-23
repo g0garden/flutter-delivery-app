b8bb5ca7d10b8423f6aa8f44f2de28e55bb46c95
jeongwongo <20123104a@gmail.com>
2024-01-23 18:12:28 +0900

    CursorPagination 클래스로 상태 작업하기
    - 클래스로 상태구분하기
    - oop 개념 리마인드하기

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -3,8 +3,24 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+//base
+abstract class CursorPaginationBase {}
+
+//ERROR
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+//Loading
+//data is CursorPaginationLaoding 만 true여도(== 인스턴스만 체크만으로 로딩여부 확인가능)
+class CursorPaginationLaoding extends CursorPaginationBase {}
+
 @JsonSerializable(genericArgumentFactories: true)
-class CursorPagination<T> {
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
   //final List<RestaurantModel> data; //이러면 레스토랑모델 타입만 받으니까 다른 pagination할때는?!
@@ -25,4 +41,22 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+//새로 고침하기(즉, 이미 데이터 받아왔고 그 후에 다시 새로고침하는 경우)
+class CursorPaginationRefetching extends CursorPagination {
+  //CursorPaginationRefetching는 CursorPagination을 상속하고, (instance is CursorPagination)
+  //CursorPagination이 상속한 CursorPaginationBase도 상속한다. (instance is CursorPaginationBase)
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+//리스트의 맨 아래로 내려서 추가 데이터 요청중일떄
+class CursorPaginationFetchingMore extends CursorPagination {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
