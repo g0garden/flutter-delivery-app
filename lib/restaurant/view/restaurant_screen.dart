9ecba59aa3b5fc1cfdd355b15712a8f7f351c1e1
jeongwongo <20123104a@gmail.com>
2024-01-24 02:56:42 +0900

    완성된 pagination 로직 실행하기
    - consumerstatefulWidget으로 변경
    - scrollcontroller 활용해서 스크롤위치가 조건 충족하면 fetchMore하도록 처리

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,18 +5,50 @@ import 'package:flutter_delivery_app/restaurant/provider/restaurant_provider.dar
 import 'package:flutter_delivery_app/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    //현재위치가 최대길이보다 조금 덜되는 위치까지 왔다면 새로운 데이터 추가요청하기
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
     if (data is CursorPaginationLaoding) {
       return const Center(child: CircularProgressIndicator());
     }
 
-    //TODO : 임시!
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    //CursorPagination
+    //CursorPaginationFetching
+    //CursorPaginationRefetching
+
     final cp = data as CursorPagination;
 
     return Padding(
