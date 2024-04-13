b4075ec932b435cd0e510247c6e415c99fd104a4
jeongwongo <20123104a@gmail.com>
2024-04-14 02:41:50 +0900

    pagination provider 일반화
    -IBasePagiantion 클래스만들어서 일반화

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -3,6 +3,7 @@ import 'package:flutter_delivery_app/common/const/data.dart';
 import 'package:flutter_delivery_app/common/dio/dio.dart';
 import 'package:flutter_delivery_app/common/model/cursor_pagination_model.dart';
 import 'package:flutter_delivery_app/common/model/pagination_params.dart';
+import 'package:flutter_delivery_app/common/repository/base_paginatioin_repository.dart';
 import 'package:flutter_delivery_app/restaurant/model/restaurant_detail_model.dart';
 import 'package:flutter_delivery_app/restaurant/model/restaurant_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,7 +22,8 @@ final restaurantRepositoryProvider = Provider<RestaurantRepository>((ref) {
 });
 
 @RestApi()
-abstract class RestaurantRepository {
+abstract class RestaurantRepository
+    implements IBasePaginationRepository<RestaurantModel> {
   //http://$ip/restaurant
   //왜 dio를 받아오냐면 =>
   factory RestaurantRepository(Dio dio, {String baseUrl}) =
